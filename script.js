9a328211b0031f0bfd2a277297e0240714a09dad
anasofich <castellanos_chopas@hotmail.com>
2021-09-06 23:06:09 -0500

    tried to calculate rgb

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 "use strict";
 
 let colorInput = document.querySelector("#colorSelector");
+let hexCode = document.querySelector(".hexColor");
+let rgbColor = document.querySelector(".rgbColor");
+let hslColor = document.querySelector(".hslColor");
 
-colorInput.addEventListener("input", () => {
-  document.querySelector(".hexColor").innerHTML = colorInput.value;
-  document.querySelector(".rgbColor").innerHTML = colorInput.value;
-  document.querySelector(".hslColor").innerHTML = colorInput.value;
-});
+colorInput.addEventListener("input", showColorData);
+
+function showColorData() {
+  hexCode.innerHTML = colorInput.value;
+  document.querySelector(".colorBox").style.backgroundColor = colorInput.value;
+  //rgbColor.innerHTML = hexToRGB();
+
+  const validHEXInput = colorInput.value;
+  const output = {
+    r: parseInt(validHEXInput[1], 16),
+    g: parseInt(validHEXInput[2], 16),
+    b: parseInt(validHEXInput[3], 16),
+  };
+
+  rgbColor.innerHTML = output;
+
+  hslColor.innerHTML = hexToRGB();
+}
+
+function hexToRGB(hex) {
+  const validHEXInput = colorInput.value;
+  const output = {
+    r: parseInt(validHEXInput[1], 16),
+    g: parseInt(validHEXInput[2], 16),
+    b: parseInt(validHEXInput[3], 16),
+  };
+  return output;
+}
